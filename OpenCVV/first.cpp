113311f0036d492f3bf571e6e10a9631da4c9f75
Iamnvincible <ljlovemichael@hotmail.com>
2014-11-26 22:17:00 +0800

    1126
    1126pro

---
+++ OpenCVV/first.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+
+
+using namespace cv;
+
+
+int main()
+{
+	// 读入一张图片（游戏原画）
+	Mat img=imread("milkyway.jpg",IMWRITE_PNG_STRATEGY_FIXED  );
+	// 创建一个名为 "游戏原画"窗口
+	imwrite("E:\\aa.jpg",img);
+	namedWindow("pic",WINDOW_NORMAL);
+	// 在窗口中显示游戏原画
+	imshow("pic",img);
+	// 等待6000 ms后窗口自动关闭
+	//printf("%d",(int)img.row);
+	waitKey(60000);
+
+}
